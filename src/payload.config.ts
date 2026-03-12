769859c9a38a8f9ba93e55c4c0ee1947670d426d
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2026-03-12 18:54:24 +0700

    Payload Integration Fix

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -7,7 +7,7 @@ import sharp from "sharp";
 
 import { Users } from "./collections/Users";
 import { Media } from "./collections/Media";
-import { Categories } from "./collections/Categoties";
+import { Categories } from "./collections/Categories";
 
 const filename = fileURLToPath(import.meta.url);
 const dirname = path.dirname(filename);
