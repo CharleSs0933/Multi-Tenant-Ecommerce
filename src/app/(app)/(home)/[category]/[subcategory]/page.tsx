6ad8856268669f8b7b2bc7d4689606c7afc9ee0e
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2026-03-17 00:36:48 +0700

    Category Pages

---
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -0,0 +1,11 @@
+interface Props {
+  params: Promise<{ category: string; subcategory: string }>;
+}
+
+const SubCategoryPage = async ({ params }: Props) => {
+  const { category, subcategory } = await params;
+
+  return <div>SubCategoryPage</div>;
+};
+
+export default SubCategoryPage;
