46c9677aad3b2e0862b9976056f566256c3c9b6a
Nguyễn Kiều Anh Quân <168531710+CharleSs0933@users.noreply.github.com>
2026-03-17 19:27:37 +0700

    Products

--- src/app/(app)/(home)/[category]/[subcategory]/page.tsx
+++ src/app/(app)/(home)/[category]/[subcategory]/page.tsx
@@ -1,11 +1,30 @@
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import ProductList, {
+  ProductListSkeleton,
+} from "@/modules/products/ui/components/product-list";
+import { Suspense } from "react";
+
 interface Props {
-  params: Promise<{ category: string; subcategory: string }>;
+  params: Promise<{ subcategory: string }>;
 }
 
-const SubCategoryPage = async ({ params }: Props) => {
-  const { category, subcategory } = await params;
+const CategoryPage = async ({ params }: Props) => {
+  const { subcategory } = await params;
 
-  return <div>SubCategoryPage</div>;
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.products.getMany.queryOptions({ category: subcategory }),
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<ProductListSkeleton />}>
+        <ProductList category={subcategory} />
+      </Suspense>
+    </HydrationBoundary>
+  );
 };
 
-export default SubCategoryPage;
+export default CategoryPage;
--- src/app/(app)/(home)/[category]/page.tsx
+++ src/app/(app)/(home)/[category]/page.tsx
@@ -1,4 +1,10 @@
-import React from "react";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+
+import ProductList, {
+  ProductListSkeleton,
+} from "@/modules/products/ui/components/product-list";
+import { Suspense } from "react";
 
 interface Props {
   params: Promise<{ category: string }>;
@@ -7,7 +13,18 @@ interface Props {
 const CategoryPage = async ({ params }: Props) => {
   const { category } = await params;
 
-  return <div>CategoryPage</div>;
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(
+    trpc.products.getMany.queryOptions({ category }),
+  );
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<ProductListSkeleton />}>
+        <ProductList category={category} />
+      </Suspense>
+    </HydrationBoundary>
+  );
 };
 
 export default CategoryPage;
